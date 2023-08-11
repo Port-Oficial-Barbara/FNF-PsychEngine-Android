fcc93cf9534fab2848a1c602e4cb5b78cdd6e721
BarbaraOficial <138633318+BarbaraOficial@users.noreply.github.com>
2023-08-10 21:05:56 -0300

    Add files via upload

--- source/import.hx
+++ source/import.hx
@@ -1,4 +1,4 @@
-
+#if !macro
 //Discord API
 #if desktop
 import backend.Discord;
@@ -8,6 +8,10 @@ import backend.Discord;
 #if LUA_ALLOWED
 import llua.*;
 import llua.Lua;
+#end
+
+#if ACHIEVEMENTS_ALLOWED
+import backend.Achievements;
 #end
 
 import backend.Paths;
@@ -21,6 +25,7 @@ import backend.Conductor;
 import backend.BaseStage;
 import backend.Difficulty;
 import backend.Mods;
+import backend.SUtil;
 
 import objects.Alphabet;
 import objects.BGSprite;
@@ -47,3 +52,4 @@ import flixel.group.FlxSpriteGroup;
 import flixel.group.FlxGroup.FlxTypedGroup;
 
 using StringTools;
+#end
